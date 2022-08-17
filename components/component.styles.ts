5b0c3c01187c8e93ceb4eec77bb268f69d4d8da3
olashina201 <olashina201@gmail.com>
2022-08-17 09:56:57 +0100

    mobile responseive

--- components/component.styles.ts
+++ components/component.styles.ts
@@ -5,8 +5,8 @@ const styles = {
     |--------------------------------------------------------------------------
     */
     cardsSection: `bg-white`,
-    cardsWrapper: `w-full px-5 py-6 mx-auto space-y-5 sm:py-8 md:py-12 sm:space-y-8 md:space-y-16 max-w-7xl`,
-    cardsContainer: `flex grid grid-cols-12 pb-10 sm:px-5 gap-x-8 gap-y-16`,
+    cardsWrapper: `w-full px-5 py-6 sm:py-8 md:py-12 sm:space-y-8 md:space-y-16 max-w-7xl`,
+    cardsContainer: `flex grid grid-cols-12 pb-10 `,
 
     /*
     |--------------------------------------------------------------------------
